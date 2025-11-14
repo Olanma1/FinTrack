6f85d757c351606f2d2f1e78375686c363ec056d
Obasi, Mary Olanma <olanmamary2@gmail.com>
2025-11-14 21:22:26 +0100

    Add Fintrack branding

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,3 @@
+<td class="footer" style="text-align:center; padding: 25px 0; color:#888;">
+    © {{ date('Y') }} FinTrack. Secure financial tracking made simple.
+</td>
---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,5 @@
+<tr>
+    <td class="header" style="text-align: center; padding: 20px 0;">
+        <strong>FinTrack</strong>
+    </td>
+</tr>
