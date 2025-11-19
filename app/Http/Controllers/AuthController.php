78cc2eb8cc0d6f04ad9e5d51f072c09dc07cac75
Obasi, Mary Olanma <olanmamary2@gmail.com>
2025-11-19 17:31:49 +0100

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,6 +17,7 @@ use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 
 class AuthController extends Controller
 {
+    
     public function register(RegisterRequest $request)
     {
         $user = User::create([
@@ -153,6 +154,17 @@ class AuthController extends Controller
 
     public function updateProfile(Request $request)
     {
+        Cloudinary::config([
+            'cloud' => [
+                'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+                'api_key' => env('CLOUDINARY_API_KEY'),
+                'api_secret' => env('CLOUDINARY_API_SECRET')
+            ],
+            'url' => [
+                'secure' => true
+            ]
+        ]);
+
         $user = $request->user();
 
         $validated = $request->validate([
